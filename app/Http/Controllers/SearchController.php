4356b5e8537143620e300b20231e0dc8fce52aee
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-13 17:06:52 +0200

    update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,35 +11,36 @@ class SearchController extends Controller
 	public function search()
 	{
 		ini_set('memory_limit', '-1');
-		
-		$rhymeClassation = array();
+
+        $rhymeClassation = array();
+
 		$getWord = Input::get('word');
 		$getWordCombinations = $this->wordCombinations($getWord);
 
 		$dbWords = Cache::rememberForever('words', function () {
 			return Word::all();
 		});
-		
+
 		foreach ($dbWords as $word) {
-			
+
 			$dbWordCombinations = $this->wordCombinations($word->word, 3);
-			
+
 			$wordCombination = mb_strtolower(end($getWordCombinations));
 			$dbWordCombination = mb_strtolower(end($dbWordCombinations));
-			
+
 			if ($wordCombination == $dbWordCombination) {
 				$rhymeClassation[] = array(
 					'word'=>$word->word,
 					'level'=>1
 				);
 			}
-			
+
 			/* foreach ($getWordCombinations as $wordCombination) {
 				foreach($dbWordCombinations as $dbWordCombination) {
-					
+
 					$wordCombination = mb_strtolower($wordCombination);
 					$dbWordCombination = mb_strtolower($dbWordCombination);
-					
+
 					if ($wordCombination == $dbWordCombination) {
 						$rhymeClassation[] = array(
 							'word'=>$word->word,
@@ -50,59 +51,59 @@ class SearchController extends Controller
 				}
 			} */
 		}
-		
+
 		return view('welcome', ['results'=>$rhymeClassation]);
 	}
-	
-	
+
+
 	private function wordCombinations($word, $combinationNumbers = 3) {
-		
+
 		$combinations = array();
-		
+
 		$alphabets = $this->split($word);
 		$i=0;
 		foreach ($alphabets as $alpha) {
 			$i++;
-			
+
 			if (!isset($alphabets[$i])) {
 				continue;
 			}
-			
+
 			if ($combinationNumbers > 2) {
 				if (!isset($alphabets[$i+1])) {
 					continue;
 				}
 			}
-			
+
 			if ($combinationNumbers > 3) {
 				if (!isset($alphabets[$i+2])) {
 					continue;
 				}
 			}
-			
+
 			if ($combinationNumbers > 4) {
 				if (!isset($alphabets[$i+3])) {
 					continue;
 				}
 			}
-			
+
 			$readyAlpha = $alpha . $alphabets[$i];
-			
+
 			if ($combinationNumbers > 2) {
 				$readyAlpha .= $alphabets[$i+1];
 			}
-			
+
 			if ($combinationNumbers > 3) {
 				$readyAlpha .= $alphabets[$i+2];
 			}
-			
+
 			if ($combinationNumbers > 4) {
 				$readyAlpha .= $alphabets[$i+3];
 			}
-			
+
 			$combinations[] = $readyAlpha;
 		}
-		
+
 		return $combinations;
 	}
 
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'SearchController@search');
 Route::get('/search', 'SearchController@search');
